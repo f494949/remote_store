7d22782249cfddc0103871a7dd3307601a937d23
f494949 <3141233476@qq.com>
2023-03-21 20:44:39 +0800

    题解

--- leetcode/1012.至少有-1-位重复的数字.js
+++ leetcode/1012.至少有-1-位重复的数字.js
@@ -4,6 +4,8 @@
  * [1012] 至少有 1 位重复的数字
  */
 
+const { default: cluster } = require("cluster");
+
 // @lc code=start
 /**
  * @param {number} n
@@ -16,7 +18,7 @@
 var numDupDigitsAtMostN = function(n) {
     const cumulate = (num) => {
         let total = 1
-        for (let i = 9; i > 9-num; i--) {
+        for (let i = 2; i <= num; i++) {
             total *= i
         }
         return total
@@ -27,24 +29,42 @@ var numDupDigitsAtMostN = function(n) {
     let count = 0
     let length = n.toString().length
     for (let i = 1; i < length; i++) {
-        count += 9 * cumulate(length-i-1)        
+        count += 9 * cumulate(9) /cumulate(9-i+1)        
     }
 
     //位数等于n的个数
-    //n=9527
-    let num = n
-    let dig = new Set
+    //n=9527,length=4
+    //第一轮 i=3，single=9,j=1   
+    let num = n 
+    let digital = new Set()
     for (let i = length-1; i >= 0 ; i--) {
-        let equal = Math.floor(num / Math.pow(10, i))
-        for (let j = i === length-1 ? 1 : 0; j < 0; j++) {
-            if (!dig.has(equal)) {}
+        let single = Math.floor(num / Math.pow(10, i))
+        for (let j = i === length-1 ? 1 : 0; j < single; j++) {
+            if (!digital.has(j)) {
+                count += cumulate(10-length+i) / cumulate(10-length)
+            }
         }
+        if (digital.has(single)) {
+            break
+        }
+        digital.add(single)
+        num -= single * Math.pow(10, i)
     }
 
-    
-
-
     //n本身为重复数
+    let self = false // if the number itself has duplicate digits
+    let used = new Set()
+    for (let i = 0; i < length; i++) {
+      let digital = Math.floor(n / Math.pow(10, i)) % 10
+      if (used.has(digital)) {
+        self = true
+        break
+      }
+      used.add(digital)
+    }
+    if (!self) {
+      count++
+    }
 
     //输出重复数的个数
     return n - count
