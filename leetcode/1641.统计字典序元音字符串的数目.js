1c710e1ba1e5d71aff801313e5052430f6df63a8
f494949 <3141233476@qq.com>
2023-03-29 11:52:10 +0800

    2023-3-29

--- leetcode/1641.统计字典序元音字符串的数目.js
+++ leetcode/1641.统计字典序元音字符串的数目.js
@@ -13,7 +13,7 @@
 // n = 2时，输出为5+4+3+2+1=15
 // n = 3时，输出为15+10+6+3+1=35
 // n = 4时，输出为35+20+10+4+1=70
-
+// 法一
 var countVowelStrings = function (n) {
   a = e = i = o = u = 1
   for (let k = 1; k < n; k++) {
@@ -24,4 +24,10 @@ var countVowelStrings = function (n) {
   }
   return a + e + i + o + u
 }
+
+// 法二
+var countVowelStrings = function(n) {
+    return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+};
+
 // @lc code=end
